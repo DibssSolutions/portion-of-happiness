7fa25a1bda3ee5373be906b20debbdfa1ebf677f
Ihor Liubko <liub41k69@gmail.com>
2019-02-18 17:36:30 +0200

    mob menu ready to review

--- src/js/components/_menu.js
+++ src/js/components/_menu.js
@@ -1,11 +1,35 @@
-import {OPEN} from '../constants';
+import {OPEN, ACTIVE, BODY, OVERFLOW_HIDDEN} from './../constants';
 
-const menu = $('.js-toggle');
+const burger = $('.js-toggle');
+const menuMob = $('.js-mob-menu');
+const icon = $('.js-toggle-icon');
 
-menu.on('click', function() {
-  const header = $('.js-header');
-  const icon = $('.js-toggle-icon');
-  icon.toggleClass(OPEN);
-  header.addClass(OPEN);
+burger.on('click', function(e) {
+  e.preventDefault();
+  icon.addClass(OPEN);
+  menuMob.addClass(OPEN);
+  e.stopPropagation();
 });
+
+menuMob.on('click', function(e) {
+  e.stopPropagation();
+});
+
+BODY.on('click', () => {
+  icon.removeClass(OPEN);
+  menuMob.removeClass(OPEN);
+});
+
+// const menuItem = menuMob.find('.mob-menu__item');
+// menuItem.each((i, item) => {
+//   item = $(item);
+//   item.on('click', function() {
+// 	  if (item.hasClass(OPEN)) {
+// 		  item.removeClass(OPEN);
+// 	  } else {
+// 		  menuItem.removeClass(OPEN);
+// 		  item.addClass(OPEN);
+// 	  }
+//   });
+// });
 
