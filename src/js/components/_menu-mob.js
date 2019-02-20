37b539647f5ff6cf68356772b36fa534000b3825
Ihor Liubko <liub41k69@gmail.com>
2019-02-20 16:10:31 +0200

    review fix

--- src/js/components/_menu-mob.js
+++ src/js/components/_menu-mob.js
@@ -3,34 +3,25 @@ import {OPEN, ACTIVE, BODY, OVERFLOW_HIDDEN} from './../constants';
 const burger = $('.js-toggle');
 const menuMob = $('.js-mob-menu');
 const icon = $('.js-toggle-icon');
-
-burger.on('click', function(e) {
-  e.preventDefault();
-  icon.addClass(OPEN);
-  menuMob.addClass(OPEN);
-  BODY.addClass(OVERFLOW_HIDDEN);
-  e.stopPropagation();
-});
-
-menuMob.on('click', function(e) {
-  e.stopPropagation();
-});
-
-BODY.on('click', () => {
+const hide = () => {
   icon.removeClass(OPEN);
   menuMob.removeClass(OPEN);
   BODY.removeClass(OVERFLOW_HIDDEN);
+};
+
+burger.on('click', function(e) {
+  e.preventDefault();
+  if (menuMob.hasClass(OPEN)) {
+    hide();
+  } else {
+	  icon.addClass(OPEN);
+	  menuMob.addClass(OPEN);
+	  BODY.addClass(OVERFLOW_HIDDEN);
+  }
 });
 
-// const menuItem = menuMob.find('.mob-menu__item');
-// menuItem.each((i, item) => {
-//   item = $(item);
-//   item.on('click', function() {
-// 	  if (item.hasClass(OPEN)) {
-// 		  item.removeClass(OPEN);
-// 	  } else {
-// 		  menuItem.removeClass(OPEN);
-// 		  item.addClass(OPEN);
-// 	  }
-//   });
-// });
+
+BODY.on('click', e => {
+  if ($(e.target).closest(menuMob).length || $(e.target).closest(burger).length) return;
+  hide();
+});
