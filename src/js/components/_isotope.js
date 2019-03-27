f9fbf1070e2e08f3e694173f4e4ce8bce9e9523a
Ihor Liubko <liub41k69@gmail.com>
2019-03-27 17:59:59 +0200

    blog started

--- src/js/components/_isotope.js
+++ src/js/components/_isotope.js
@@ -2,17 +2,18 @@ import Isotope from 'isotope-layout';
 
 
 const masonry = '.js-gallery';
-if (document.querySelector(masonry)) {
-  var iso;
+if (document.querySelectorAll(masonry)) {
 
-  new Isotope(masonry, {
-	  itemSelector: '.js-gallery-item',
-	  layoutMode: 'fitRows',
-	  percentPosition: true
-    //   masonry: {
-    // 	// use element for option
-    // 	columnWidth: '.grid-sizer'
-    //   }
+  document.querySelectorAll(masonry).forEach( el => {
+	  new Isotope(el, {
+		  itemSelector: '.js-gallery-item',
+		  layoutMode: 'fitRows',
+		  percentPosition: true
+      //   masonry: {
+      // 	// use element for option
+      // 	columnWidth: '.grid-sizer'
+      //   }
+	  });
   });
 };
 
