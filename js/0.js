cdb52aef812e538460aa4bdd321aee3a74e46e3d
Valerii Derevshchykov <kaktuz34@gmail.com>
2019-04-22 16:44:37 +0300

    added adaptive and start order

--- js/0.js
+++ js/0.js
@@ -1,1 +1,1 @@
-webpackJsonp([0],{44:function(n,o){function e(n){throw new Error("Cannot find module '"+n+"'.")}e.keys=function(){return[]},e.resolve=e,n.exports=e,e.id=44}});
+webpackJsonp([0],{52:function(n,o){function e(n){throw new Error("Cannot find module '"+n+"'.")}e.keys=function(){return[]},e.resolve=e,n.exports=e,e.id=52}});
